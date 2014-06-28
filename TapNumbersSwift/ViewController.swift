d86204c79dd6520a1884ccc5e58cb8aeef075936
Ken Tominaga <ken.for.develop@gmail.com>
2014-06-28 14:59:24 +0900

    Initial Commit

---
+++ TapNumbersSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapNumbersSwift
+//
+//  Created by Ken Tominaga on 6/28/14.
+//  Copyright (c) 2014 Tommy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
