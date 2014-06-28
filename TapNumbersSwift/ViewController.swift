755409ccfc9a45e746ad892241b5165ee05dc3e7
Ken Tominaga <ken.for.develop@gmail.com>
2014-06-28 16:07:41 +0900

    First commit

--- TapNumbersSwift/ViewController.swift
+++ TapNumbersSwift/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var combinationLabel: UILabel
+    var combination: Int[] = [0, 0, 0, 0]
+    // var combination: Int[4] = Int[4]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        self.newGame()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func newGame() {
+        combination = self.produceCombination()
+        combinationLabel.attributedText = self.makeStringFromArray(combination)
+    }
+    
+    func makeStringFromArray(array: Array<Int>) -> NSAttributedString {
+        var labelText: NSMutableAttributedString = NSMutableAttributedString()
+        
+        for var i: Int = 0; i < array.count; i++ {
+            
+            if i == 0 {
+                labelText.appendAttributedString(self.highlightTargetNumber(array[i]))
+            } else {
+                let attributedString = NSAttributedString(string: "\(array[i])")
+                labelText.appendAttributedString(attributedString)
+            }
+            
+        }
+        
+        return labelText
+    }
+    
+    
+    func highlightTargetNumber(number: Int) -> NSAttributedString {
+        return NSAttributedString(string: "\(number)", attributes: [NSForegroundColorAttributeName: UIColor.redColor()])
+    }
+    
+    func produceCombination() -> Int[] {
 
+        combination = [0, 0, 0, 0]
+        
+        for var i: Int = 0; i < 4; i++ {
+            combination[i] = Int(arc4random_uniform(UInt32(4))) + 1
+        }
+        
+        return combination;
+    }
+    
+    @IBAction func buttonPressed(button: UIButton) {
+        if combination[0] == button.tag {
+            combination.removeAtIndex(0)
+            combinationLabel.attributedText = self.makeStringFromArray(combination)
+        }
+        if combination.count == 0 {
+            self.newGame()
+        }
+    }
 }
 
